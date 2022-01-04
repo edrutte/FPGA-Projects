95625de8c75657487f11bdde94504829ed9a99c6
Evan R <70585539+edrutte@users.noreply.github.com>
2022-01-04 12:58:45 -0500

    Update vitis scripts

--- SHA256/Vitis/First/script.tcl
+++ SHA256/Vitis/First/script.tcl
@@ -7,10 +7,10 @@ open_project Vitis
 set_top sha256chunk
 add_files Vitis/csrc/shalibHLS.c
 open_solution "First" -flow_target vivado
-set_part {xc7a100tftg256-1}
+set_part {xc7a100t-ftg256-1}
 create_clock -period 20 -name default
 config_export -vivado_clock 20
-#source "./Vitis/First/directives.tcl"
+source "./Vitis/First/directives.tcl"
 #csim_design -ldflags {-m64 -B/usr/lib/x86_64-linux-gnu}
 csynth_design
 #cosim_design -ldflags {-m64 -B/usr/lib/x86_64-linux-gnu}
