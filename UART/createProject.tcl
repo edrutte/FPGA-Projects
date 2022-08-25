5f37b47a338520e2d97f5e79bcc00058fedc88f3
Evan R <70585539+edrutte@users.noreply.github.com>
2022-08-25 16:38:59 -0400

    Convert UART to createProject.tcl

---
+++ UART/createProject.tcl
@@ -0,0 +1,15 @@
+proc init {} {
+    create_project -part xc7a100tftg256-1 UART
+    config_webtalk -user off
+    set_property target_language VHDL [current_project]
+    set_property simulator_language Mixed [current_project]
+}
+
+proc setupFiles {} {
+    add_files "src/sources_1/"
+    add_files -fileset constrs_1 "src/constrs_1/"
+    add_files -fileset sim_1 "src/sim_1/"
+}
+
+init
+setupFiles
